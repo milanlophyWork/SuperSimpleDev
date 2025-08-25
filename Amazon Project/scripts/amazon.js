71793c6baf21756e8c352173331dcd5a1b4a58b0
milanlophy <milanlophy@gmail.com>
2025-08-25 11:56:28 +0530

    Generate html with JS

--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -1,1 +1,103 @@
 console.log('hello')
+
+// Adding more products, JS steps : 1) Save the data 2) Generate the html 3) Make it interactive
+
+// data means information. In this case data is information about our products. we are creating something in JS that closely matches the data on the page
+
+/* create a variable to save the data. Our data is a list of products so we save it in an array. Because array matches a list.
+   Each product in the list has many values like img, name, price, rating, etc. So we use an object to represent each object. Because object group multiple values together. */
+
+const products = [{ // This is called a data structure. Because it structures or organizes the data.
+    img : 'img/products/athletic-cotton-socks-6-pairs.jpg',
+    name : 'Black and Gray Atheletic Cotton Socks - 6 Pairs',
+    rating : {
+        stars : 4.5,
+        count : 87
+    },
+    priceCents : 1090
+
+},
+{
+    img : 'img/products/intermediate-composite-basketball.jpg',
+    name : 'Intermediate Size Basketball',
+    rating : {
+        stars : 4,
+        count : 127
+    },
+    priceCents : 2095
+},
+{
+    img : 'img/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name : 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating : {
+        stars : 4.5,
+        count : 56
+    },
+    priceCents : 799
+},
+
+] 
+
+// next step is to use this data to generate the html. To generate the html we loop this array.
+
+let productsHTML = '' // after generating the html we want to combine the all html together into a string. Create a variable for the same
+
+products.forEach((product)=> {
+    const html = `
+        <div class= "product-container">
+            <div class= "product-image-container">
+                <img src = "${product.img}" class= "product-image">
+            </div>
+
+            <div class = "product-name limit-text-to-2-lines">
+                ${product.name}
+            </div>
+
+            <div class= "product-rating-container">
+                <img src = "img/ratings/rating-${product.rating.stars * 10}.png" class= "product-rating-stars">
+                
+                <div class = "product-rating-count link-primary">
+                    ${product.rating.count}
+                </div>
+
+            </div>
+
+            <div class = "product-price">
+                $${(product.priceCents/ 100).toFixed(2)} 
+            </div>
+
+            <div class = "product-quantity-container">
+                <select>
+                    <option value = "1" selected>1</option>
+                    <option value = "2">2</option>
+                    <option value = "3">3</option>
+                    <option value = "4">4</option>
+                    <option value = "5">5</option>
+                    <option value = "6">6</option>
+                    <option value = "7">7</option>
+                    <option value = "8">8</option>
+                    <option value = "9">9</option>
+                    <option value = "10">10</option>
+                </select>
+            </div>
+
+            <div class = "product-spacer"></div>
+
+            <div class= "added-to-cart">
+                <img src="img/icons/checkmark.png" >
+                Added
+            </div>
+
+            <button class = "add-to-cart-button button-primary">
+                Add to Cart
+            </button>
+        </div>
+    ` // creating html for each of the product , here .toFixed() is a method for numbers that lets us specify the number of decimals we want. toFixed() will convert a number into a string.
+
+    productsHTML += html // Accumulator pattern    
+})
+
+// last step- make it interactive, ie put it on the webpage using the DOM
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML
+
