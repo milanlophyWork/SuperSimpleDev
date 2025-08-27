86112b5919ff05cd68b086e4cecf943149272550
milanlophy <milanlophy@gmail.com>
2025-08-27 09:18:26 +0530

    Generated html for orderSummary and made interactive

--- lesson 14/14_Amazon Project with modules/data/cart.js
+++ lesson 14/14_Amazon Project with modules/data/cart.js
@@ -1,5 +1,12 @@
 
-export const cart = [] // now this variable can be used outside cart.js
+export const cart = [{
+    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", // deduplicating the data or normalizing the data : Using id we could access all other properties . So no need to store details again 
+    quantity : 2
+},
+{
+    productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity : 1
+}] // now this variable can be used outside cart.js
 
 
 export function addToCart(productId){
---
+++ lesson 14/14_Amazon Project with modules/scripts/checkout.js
@@ -0,0 +1,112 @@
+/* Has 2 sections cart-section and payment-section 
+   Main idea of JavaScript : 
+   1. Save the data 2. Generate the HTML 3. Make it interactive  */
+
+import { cart } from "../data/cart.js";
+import {products} from '../data/product.js'; // data saved earlier imported
+
+let cartSummaryHTML = ''
+
+cart.forEach((cartItem)=> {
+
+    const productId = cartItem.productId // normalizing // using id, to navigate through all properties we want
+
+    let matchingProduct
+
+    products.forEach((product)=> {
+        if(product.id === productId){
+            matchingProduct = product
+        }
+    });
+
+    console.log(matchingProduct);
+
+    cartSummaryHTML += `
+        <div class= "cart-item-container">
+            <div class= "delivery-date">
+
+            </div>
+
+            <div class= "cart-item-details-grid">
+                <img src = "${matchingProduct.img}" class= "product-image">
+                
+                <div class= "cart-item-details">
+                    <div class= "product-name">
+                        ${matchingProduct.name}
+                    </div>
+
+                    <div class= "product-prize">
+                        ${matchingProduct.priceCents / 100}
+                    </div>
+
+                    <div class= "product-quantity">
+                        <span>
+                            Quantity: <span class= "quantity-label">${cartItem.quantity}</span>
+                        </span>
+
+                        <span class="update-quantity-link link-primary">
+                            Update
+                        </span>
+
+                        <span class="delete-quantity-link link-primary">
+                            Delete
+                        </span>
+                    </div>
+                </div>
+
+                <div class= "delivery-options">
+                    <div class= "delivery-options-title">
+                        Choose a delivery option:
+                    </div>
+
+                    <div class= "delivery-option">
+                        <input type = "radio" class="delivery-option-input" name= "delivery-option-1">
+
+                        <div>
+                            <div class= "delivery-option-date">
+                                Tuesday, June 21
+                            </div>
+
+                            <div class="delivery-option-price">
+                                FREE Shipping
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class= "delivery-option">
+                        <input type = "radio" class= "delivery-option-input" name="delivery-option-1">
+
+                        <div>
+                            <div class= "delivery-option-date">
+                                Wednesday, June 15
+                            </div>
+
+                            <div class= "delivery-option-price">
+                                $4.99 - Shipping
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class= "delivery-option">
+                        <input type= "radio" class= "delivery-option-input" name= "delivery-option-1">
+                        
+                        <div>
+                            <div class= "delivery-option-date">
+                                Monday, June 13
+                            </div>
+
+                            <div class= "delivery-option-price">
+                                $9.99 - Shipping
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+
+
+})
+
+document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
+console.log(cartSummaryHTML)
