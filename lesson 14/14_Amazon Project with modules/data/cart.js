b746699f4f03eaf7091b924fead3f2840bffd051
milanlophy <milanlophy@gmail.com>
2025-08-27 12:14:41 +0530

    Save Cart to localStaorage

--- lesson 14/14_Amazon Project with modules/data/cart.js
+++ lesson 14/14_Amazon Project with modules/data/cart.js
@@ -1,13 +1,17 @@
 
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
     productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", // deduplicating the data or normalizing the data : Using id we could access all other properties . So no need to store details again 
     quantity : 2
 },
 {
     productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity : 1
-}] // now this variable can be used outside cart.js
+}] // now this variable can be used outside cart.js 
+// cart as variable will reset on refreshing the page or on moving to a different page. To solve this problem we can use localStorage to save our cart
 
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addToCart(productId){
     let matchingItem
@@ -26,6 +30,8 @@ export function addToCart(productId){
             quantity: 1
         })
     }
+
+    saveToStorage()
 }
 
 export function removeFromCart(productId){
@@ -38,4 +44,5 @@ export function removeFromCart(productId){
         }
     })
     cart = newCart
+    saveToStorage()
 }
