e37b45f46831347cc49a83f9d1c34a03bc9679a8
milanlophy <milanlophy@gmail.com>
2025-08-31 22:20:13 +0530

    delivery options array created

--- lesson 15/15_AmazonProject with External libraries/data/deliveryOptions.js
+++ lesson 15/15_AmazonProject with External libraries/data/deliveryOptions.js
@@ -1,16 +1,16 @@
 export const deliveryOptions = [ // step 1 : save the data
     {
-        id: '1', // normalizing the data // the given id is passed to cart to get these properties 
+        id: '01', // normalizing the data // the given id is passed to cart to get these properties 
         deliveryDays: 7,
         priceCents: 0
     },
     {
-        id : '2',
+        id : '02',
         deliveryDays : 3,
         priceCents : 499
     },
     {
-        id: '3',
+        id: '03',
         deliveryDays : 1,
         priceCents : 999
     }
